ef826520e3b932948a2a2ad4db772806a2ce58ce
Chanupa Athsara <143985270+chanupa2002@users.noreply.github.com>
2025-09-28 12:03:26 +0530

    Delete frontend directory

--- frontend/src/components/ResultsGrid.jsx
+++
@@ -1,9 +0,0 @@
-import ResultCard from "./ResultCard";
-
-export default function ResultsGrid({ results }) {
-  return (
-    <div className="grid">
-      {results.map(r => <ResultCard key={r.id || r.url} doc={r} />)}
-    </div>
-  );
-}
